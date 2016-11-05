c0ad443481ebde461c4c3ed46a7f99603c1ab9d6
David Aebersold <davidaebersold@gmail.com>
2016-11-05 16:25:35 -0400

    Initial commit. 2 Hours dev investment thus far.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # FlowerExchange
 A restful api for all floral shops big and small facilitating order taking and making.
+
+
+To get started:
+
+git clone 
+npm install -g swagger
+cd flower-exchange
+
+
+To run editor: 
+Editor: swagger project edit
+
+To run the webAPI:
+RUN:  swagger project start flower-exchange
---
+++ app.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var SwaggerExpress = require('swagger-express-mw');
+var app = require('express')();
+module.exports = app; // for testing
+
+var config = {
+  appRoot: __dirname, // required config
+  swaggerSecurityHandlers: {
+    api_key: function (req, authOrSecDef, scopesOrApiKey, callback) {
+      // your security code
+      if ('1234' === scopesOrApiKey) {
+        callback(null);
+      } else {
+        callback(new Error('access denied!'));
+      }
+    }
+  }
+};
+
+
+
+SwaggerExpress.create(config, function(err, swaggerExpress) {
+  if (err) { throw err; }
+
+  // install middleware
+  swaggerExpress.register(app);
+
+  var port = process.env.PORT || 10010;
+  app.listen(port);
+
+  if (swaggerExpress.runner.swagger.paths['/orders']) {
+    console.log('try this:\ncurl http://127.0.0.1:' + port + '/orders?zipCode=47150&radius=50');
+  }
+  if (swaggerExpress.runner.swagger.paths['/order']) {
+    console.log('try this:\ncurl http://127.0.0.1:' + port + '/order?orderId=10492939');
+  }
+  if (swaggerExpress.runner.swagger.paths['/account']) {
+    console.log('try this:\ncurl http://127.0.0.1:' + port + '/account?accountId=124');
+  }
+  if (swaggerExpress.runner.swagger.paths['/order/create']) {
+    console.log('try this:\ncurl http://127.0.0.1:' + port + '/order/create');
+  }
+});
