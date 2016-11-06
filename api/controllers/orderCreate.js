0479ff0ea775bc7904d1d68e706ed64078a448b5
David Aebersold <davidaebersold@gmail.com>
2016-11-06 00:32:31 -0400

    Added saving orders sample.

--- api/controllers/orderCreate.js
+++ api/controllers/orderCreate.js
@@ -5,27 +5,57 @@ var util = require('util');
 var mongoose = require('mongoose');
 var Order = require('../models/Order.js');
 
-module.exports = {
-  orderCreate: orderCreate
+var getCoords = function(){
+
+  var NodeGeocoder = require('node-geocoder');
+  
+  var options = {
+    provider: 'google',
+  
+    // Optional depending on the providers 
+    httpAdapter: 'https', // Default 
+    apiKey: 'AIzaSyDUnhPZJjoED_bQQ3tyUJ7pXXk6Z9GW-e8', // for Mapquest, OpenCage, Google Premier 
+    formatter: null         // 'gpx', 'string', ... 
+  };
+  
+  var geocoder = NodeGeocoder(options);
+  
+  // Using callback 
+  geocoder.geocode('2808 Sandalwood Dr. New Albany, IN 47150').then(function(res) {
+    console.log(res);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
 };
 
 function orderCreate(req, res) {
+  
   // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
-  console.log("order",req.body);
   var orderBody = req.body || 'no order given';
   var orderDetail = util.format('Order: %j!', orderBody);
-
+  var orderToInsert = new Order(orderBody);
+  console.log("orderToInsert", orderToInsert);
+  getCoords();
+  
   // Create a order in memory
-  var order = new Order({name: 'Master', completed: false, note: 'Gtesting 1'});
+  var order = new Order(orderToInsert);
+  
   // Save it to database
   order.save(function(err){
-    if(err)
+    if(err) {
       console.log(err);
-    else
-      console.log(order);
+      res.json({stuff:err});
+    } else {
+      console.log(orderToInsert);
+      res.json(orderToInsert.toString());
+    }
   });
 
   // this sends back a JSON response which is a single string
   //res.json(orderDetail);
-}
+};
 
+module.exports = {
+  orderCreate: orderCreate
+};
